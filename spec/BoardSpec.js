1a736de4b85bba1f91572bde5ff725f3029a3815
Magnus Holm <mail@magnusholm.com>
2017-05-18 11:01:04 +0100

    Add array of squares

---
+++ spec/BoardSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Board', function() {
+
+  var board;
+
+  beforeEach(function() {
+    board = new Board();
+  })
+
+  it('has nine squares', function() {
+    expect(board.squares.length).toEqual(9);
+  });
+
+})
---
+++ src/Board.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var Board = function() {
+
+  this.squares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+}
